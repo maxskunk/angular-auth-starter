9599f9524c2117ccf6688d6ff732b9f82ded7d8b
Brian Weis <brian@brianweis.net>
2021-12-29 07:01:06 -0700

    Added tree shakable Firebase

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web';
+
+  constructor(public auth: AngularFireAuth) {
+  }
+  login() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
 }
