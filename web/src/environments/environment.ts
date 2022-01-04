f749e4fc97157ef874f65c03a8a1913717226a70
Brian Weis <brian@brianweis.net>
2022-01-04 09:06:41 -0700

    config

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -4,7 +4,17 @@
 
 export const environment = {
   production: false,
-  serviceUrl: "http://localhost:8080"
+  serviceUrl: "http://localhost:8080",
+  config: {
+    apiKey: "AIzaSyCxwy7A3zbacGJfI6XMe-B9xTobJBrhxZA",
+    authDomain: "zokya-media-auth.firebaseapp.com",
+    databaseURL: "https://zokya-media-auth.firebaseio.com",
+    projectId: "zokya-media-auth",
+    storageBucket: "zokya-media-auth.appspot.com",
+    messagingSenderId: "926675477732",
+    appId: "1:926675477732:web:eec0bb3351f62c2b8878f1",
+    measurementId: "G-9V2TWF48N3"
+  }
 };
 
 /*
